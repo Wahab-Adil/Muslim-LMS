c4445df2b9fef37f88d07cf3df54de4827efbcc0
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2024-09-02 09:33:54 -0700

    Update data.jsx
    advertisement section file edited

--- frontend/src/data.jsx
+++ frontend/src/data.jsx
@@ -1,21 +1,24 @@
-import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined';
-import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
-import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+import VideocamOutlinedIcon from "@mui/icons-material/VideocamOutlined";
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
 
-export const Homeextra_1 = [
-    {icon:<VideocamOutlinedIcon />, title:"30,000 online courses",des:"Enjoy a variety of fresh topics"},
-    {icon:<PeopleAltOutlinedIcon />, title:"Expert instruction",des:"Find the right instructor for you"},
-    {icon:<AccessTimeOutlinedIcon />, title:"Lifetime access",des:"Learn on your schedule"},
-];
-
-export const home_count = [
-    {icon:<VideocamOutlinedIcon />, title:"150+",des:"Teachers"},
-    {icon:<PeopleAltOutlinedIcon />, title:"28K+",des:"Videos"},
-    {icon:<AccessTimeOutlinedIcon />, title:"9.3M+",des:"Students"},
-    {icon:<AccessTimeOutlinedIcon />, title:"4.1M+",des:"App Users"},
-];
-
-
-export const sliderData = [
-    {},
-];
+export default function (adminProfile) {
+  console.log(adminProfile);
+  return [
+    {
+      icon: <VideocamOutlinedIcon />,
+      title: adminProfile?.user?.courses?.length,
+      des: "Courses",
+    },
+    {
+      icon: <PeopleAltOutlinedIcon />,
+      title: adminProfile?.user?.articles?.length,
+      des: "Articles",
+    },
+    {
+      icon: <AccessTimeOutlinedIcon />,
+      title: adminProfile?.user?.totalUsers,
+      des: "App Users",
+    },
+  ];
+}
