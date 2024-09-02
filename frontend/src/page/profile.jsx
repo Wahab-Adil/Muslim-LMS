5c2f1065cfd5d2c975eec32db489a90ce58ac7e6
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2024-09-02 09:32:00 -0700

    dynamic api profile
    dynamic api profile

--- frontend/src/page/profile.jsx
+++ frontend/src/page/profile.jsx
@@ -7,6 +7,7 @@ import {
 import { useSelector, useDispatch } from "react-redux";
 import { FaSignOutAlt, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import baseUrl from "../utils/baseUrl";
 
 const DropdownComponent = () => {
   const dispatch = useDispatch();
@@ -23,16 +24,18 @@ const DropdownComponent = () => {
     setIsOpen(!isOpen);
   };
 
+  console.log("");
   return (
     <div className="relative inline-block">
       {/* Image that toggles the dropdown */}
       <img
         alt="User Profile"
         src={
-          !userProfile?.user === "Avatar" || !userProfile?.user === "default"
-            ? userProfile?.user?.avatar
+          userProfile?.user !== "Avatar" || userProfile?.user !== "default"
+            ? baseUrl(userProfile?.user?.avatar, 8)
             : "https://as1.ftcdn.net/v2/jpg/05/90/59/88/1000_F_590598870_TOcGd4cUZzPoEMlxSc7XYwcupHOE0vLM.jpg"
         }
+        title={userProfile?.user?.name}
         className="object-cover object-center w-10 h-10 rounded-full cursor-pointer"
         onClick={toggleDropdown}
       />
@@ -41,7 +44,7 @@ const DropdownComponent = () => {
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg p-4"
+            className="absolute right-0 mt-2 w-38 bg-white shadow-lg rounded-lg p-4"
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.9 }}
@@ -54,10 +57,10 @@ const DropdownComponent = () => {
               >
                 <Link
                   className="flex items-center p-2 rounded-lg cursor-pointer hover:bg-gray-100 transition duration-150 ease-in-out"
-                  to={"/adil"}
+                  to={`/user/${userProfile?.user?._id}`}
                 >
-                  <FaUser className="text-xl mr-2 ml-3  text-[#754FFE]" />
-                  <span>My Profile</span>
+                  <FaUser className="text-lg mr-2 ml-3  text-[#754FFE]" />
+                  <span className="text-sm">Profile</span>
                 </Link>
               </motion.li>
               <motion.li
